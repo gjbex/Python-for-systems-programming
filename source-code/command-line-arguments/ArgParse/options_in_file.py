463d3e759ff269693fabac18f96b5b36feb21180
Geert Jan Bex <geertjan.bex@uhasselt.be>
2024-04-16 09:47:54 +0200

    Fix typos

--- source-code/command-line-arguments/ArgParse/options_in_file.py
+++ source-code/command-line-arguments/ArgParse/options_in_file.py
@@ -3,14 +3,14 @@
 import argparse
 
 
-def amin():
+def main():
     arg_parser = argparse.ArgumentParser(fromfile_prefix_chars='@')
     arg_parser.add_argument('--foo', help='foo option')
-    arg_parser.add_argument('--bar', help='bar optoin')
+    arg_parser.add_argument('--bar', help='bar option')
     arg_parser.add_argument('--flag', action='store_true',
                             help='flag option')
     options = arg_parser.parse_args()
     print(options)
 
 if __name__ == '__main__':
-    amin()
+    main()
