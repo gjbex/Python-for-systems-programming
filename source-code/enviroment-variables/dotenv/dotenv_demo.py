155ee4cecf4eadcd39f3a4c50afa46d7cef2a0cb
Geert Jan Bex <geertjan.bex@uhasselt.be>
2026-01-22 10:44:47 +0100

    Add dotenv example

---
+++ source-code/enviroment-variables/dotenv/dotenv_demo.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+from dotenv import load_dotenv
+import os
+
+
+def main():
+    # Check whether the environment variable is already set
+    if 'MY_SECRET' in os.environ:
+        print('MY_SECRET is already set in the environment.')
+    else:
+        # Load environment variables from .env file
+        print('Loading environment variables from .env file...')
+        load_dotenv()
+
+    # Access an environment variable
+    secret_key = os.getenv('MY_SECRET')
+
+    if secret_key:
+        print(f'MY_SECRET_KEY: {secret_key}')
+    else:
+        print('MY_SECRET not found in environment variables.')
+
+
+if __name__ == '__main__':
+    main()
